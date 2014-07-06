7e4572c8c6ae7ebd80bfa5717246f902ff21e3e1
tegusi <tegusi@tegusitekiimac.lan>
2014-07-06 15:53:35 +0800

    Initial Commit

---
+++ Mahjong/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mahjong
+//
+//  Created by tegusi on 14-7-6.
+//  Copyright (c) 2014年 NMOI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
